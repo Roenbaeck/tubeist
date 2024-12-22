b53758f3f1a86afc3586e027d08e84c7d7c958f1
Lars Rönnbäck <lars.ronnback@anchormodeling.com>
2024-12-22 10:17:39 +0100

    Global sync of atSourceTime

--- Tubeist/AssetInterceptor.swift
+++ Tubeist/AssetInterceptor.swift
@@ -80,10 +80,10 @@ actor AssetWriterActor {
             LOG("Error starting writing: \(assetWriter.error?.localizedDescription ?? "Unknown error")")
             return
         }
-        let currentTime = CMTimeMakeWithSeconds(CACurrentMediaTime(), preferredTimescale: Int32(TIMESCALE))
+        let currentTime = CMTime(seconds: Date().timeIntervalSinceReferenceDate, preferredTimescale: Int32(TIMESCALE))
         assetWriter.startSession(atSourceTime: currentTime)
         
-        LOG("Asset writer configured successfully", level: .info)
+        LOG("Asset writer configured successfully (at source time: \(currentTime.value) | \(currentTime.timescale))", level: .info)
     }
     
      func finishWriting() async {
